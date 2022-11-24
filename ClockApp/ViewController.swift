6c86182e2febd83dc682cf08296decb9817ae467
jonatangroncode <114836936+jonatangroncode@users.noreply.github.com>
2022-11-24 17:27:32 +0100

    Initial Commit

---
+++ ClockApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClockApp
+//
+//  Created by ITHS on 2022-11-24.
+//  Copyright © 2022 ITHS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
